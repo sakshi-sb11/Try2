44d235a737a11f36558fae943a6416243748101a
sakshi-sb11 <156229526+sakshi-sb11@users.noreply.github.com>
2024-03-27 14:57:56 +0530

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello{
+  public static void main(String args[]){
+    System.out.println("**********************************");
+    System.out.println("Hello Github..");
+    System.out.println("Hello Pune");
+    System.out.println("**********************************");
+
+
+    
+  } 
+
+}
